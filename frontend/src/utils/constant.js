087d9c7d71ef9a149c9ce2c09865a2af2ddddedf
Shivam Kumar <shivamkumar@Shivams-MacBook-Air.local>
2025-08-29 12:45:12 +0530

    commit

---
+++ frontend/src/utils/constant.js
@@ -0,0 +1,4 @@
+export const USER_API_END_POINT = `${import.meta.env.VITE_API_BASE_URL}/user`;
+export const JOB_API_END_POINT = `${import.meta.env.VITE_API_BASE_URL}/job`;
+export const APPLICATION_API_END_POINT = `${import.meta.env.VITE_API_BASE_URL}/application`;
+export const COMPANY_API_END_POINT = `${import.meta.env.VITE_API_BASE_URL}/company`;
